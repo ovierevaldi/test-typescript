bfb8bf2682594a71dbfffed92d5cb752579b69bc
Ovie Revaldi <ovierevaldi@gmail.com>
2025-06-13 14:02:38 +0700

    gatu lagi lah

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,12 +1,16 @@
-import "reflect-metadata"
-import { DataSource } from "typeorm"
-import { User } from "./entity/User.js"
+import 'reflect-metadata';
+import { DataSource } from 'typeorm';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 export const AppDataSource = new DataSource({
-    type: 'postgres',
-    url: process.env.DATABASE_URL,
-    ssl: { rejectUnauthorized: false },
-    synchronize: true,
-    logging: true,
-    entities: [User]
-})
+  type: 'postgres',
+  url: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false },
+  synchronize: true,
+  logging: true,
+  entities: [__dirname + '/entity/*.ts'], // use .ts in dev
+});
